23bf9c615c27bfefede79d97dd6e5c8ed38c5c1a
krisan0709 <krisan0709@gmail.com>
2026-01-20 13:53:24 +0530

    Create db.js

---
+++ db.js
@@ -0,0 +1,19 @@
+
+// db.js
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,      // Railway Host
+  user: process.env.DB_USER,      // root
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,  // railway
+  port: process.env.DB_PORT,      // << IMPORTANT ( Railway port )
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool;
+
+
